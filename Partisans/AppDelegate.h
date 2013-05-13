3be008583082dc872df68f28003c60a8e1d30843
Joshua Kaden <rich@propeller-com.com>
2013-05-13 13:17:00 -0400

    Initial Commit

---
+++ Partisans/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Partisans
+//
+//  Created by Joshua Kaden on 5/13/13.
+//  Copyright (c) 2013 Chadford Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Partisans/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Partisans
+//
+//  Created by Joshua Kaden on 5/13/13.
+//  Copyright (c) 2013 Chadford Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
